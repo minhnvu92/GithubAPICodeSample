1d7387b55b954ca814fd40fff4bfb979cc827421
Minh Vu <Minh Vu>
2017-03-31 18:30:34 -0400

    update empty input validation

--- Script.js
+++ Script.js
@@ -2,19 +2,21 @@ var MyApp = angular.module('MyApp', []);
 
 MyApp.controller("gitController", function($scope, $http) {
     $scope.getRepos = function() {
-        //set url for HTTP request
-        var Url = "https://api.github.com/users/" + $scope.username + "/repos";
-        //use $http to pull data from the Github API using GET method
-        $http({
-                method: 'GET',
-                url: Url
-            })
-            .then(function(response) {
-                $scope.repos = response.data;
-            }).catch(function(response) {
-                alert("Error: Username not found. Please use valid username");
+        if ($scope.username) {
+            //set url for HTTP request
+            var Url = "https://api.github.com/users/" + $scope.username + "/repos";
+            //use $http to pull data from the Github API using GET method
+            $http({
+                    method: 'GET',
+                    url: Url
+                })
+                .then(function(response) {
+                    $scope.repos = response.data;
+                }).catch(function(response) {
+                    alert("Error:Username (" + $scope.username + ") not found. Please use valid username");
 
-            });
+                });
 
+        }
     };
 });
